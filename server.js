37166ebb22547c749421e4516b78a8e85a949ca8
Gijs Nijholt <gijs.nijholt@gmail.com>
2026-05-28 09:07:16 +0200

    fix

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-import { createServer } from "http";
-import { WebSocketServer } from "ws";
-import { setupWSConnection } from "y-websocket/bin/utils.js";
+const http = require('http')
+const WebSocket = require('ws')
+const { setupWSConnection } = require('y-websocket/bin/utils')
 
-const host = process.env.HOST || "0.0.0.0";
-const port = parseInt(process.env.PORT || "8080");
+const host = process.env.HOST || '0.0.0.0'
+const port = parseInt(process.env.PORT || '8080')
 
-const server = createServer((_, res) => {
-  res.writeHead(200);
-  res.end("ok");
-});
+const server = http.createServer((_, res) => { res.writeHead(200); res.end('ok') })
+const wss = new WebSocket.Server({ server })
+wss.on('connection', (ws, req) => setupWSConnection(ws, req))
 
-const wss = new WebSocketServer({ server });
-wss.on("connection", (ws, req) => setupWSConnection(ws, req));
-
-server.listen(port, host, () => {
-  console.log(`y-websocket listening on ${host}:${port}`);
-});
+server.listen(port, host, () => console.log(`listening on ${host}:${port}`))
